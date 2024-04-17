9a1bd649f109464de9481d1a03af32254411097b
btian1 <btian1@umd.edu>
2024-04-17 02:39:40 -0400

    test

--- docker.sh
+++ docker.sh
@@ -3,4 +3,3 @@
 docker run -d -t --network=host -p 80:80 --cpus 64 --memory 512G --gpus '"device=0,1"' --privileged=True --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --name fgl -v /etc/localtime:/etc/localtime continuumio/anaconda3
 docker cp . fgl:/Federated_Generative_Learning/
 docker exec -it fgl /bin/bash
-hello world
