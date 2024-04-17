c26761061b93de284fdfd4b14245ae2286f6339e
btian1 <btian1@umd.edu>
2024-04-17 09:38:30 -0400

    update

--- docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d -t --network=host -p 80:80 --cpus 64 --memory 512G --gpus '"device=0,1"' --privileged=True --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --name fgl -v /etc/localtime:/etc/localtime -v /export/btian1:/dataspace fgl_image
-docker cp . fgl:/Federated_Generative_Learning/
-docker exec -it fgl /bin/bash
---
+++ doup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -r . /export/btian1
